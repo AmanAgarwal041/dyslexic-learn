71e71da9bbe2ddc810c434e7c29679ebd417d256
codehimanshu <himanshuagrawal1998@gmail.com>
2019-11-17 00:00:53 +0530

    add ocr script

---
+++ ocr/ocr.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const request = require('request');
+
+let subscriptionKey = process.env['COMPUTER_VISION_SUBSCRIPTION_KEY'];
+let endpoint = process.env['COMPUTER_VISION_ENDPOINT']
+if (!subscriptionKey) { throw new Error('Set your environment variables for your subscription key and endpoint.'); }
+
+var uriBase = endpoint + 'vision/v2.1/ocr';
+
+const imageUrl = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/' +
+    'Atomist_quote_from_Democritus.png/338px-Atomist_quote_from_Democritus.png';
+
+// Request parameters.
+const params = {
+    'language': 'unk',
+    'detectOrientation': 'true',
+};
+
+const options = {
+    uri: uriBase,
+    qs: params,
+    body: '{"url": ' + '"' + imageUrl + '"}',
+    headers: {
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key' : subscriptionKey
+    }
+};
+
+request.post(options, (error, response, body) => {
+  if (error) {
+    console.log('Error: ', error);
+    return;
+  }
+  let jsonResponse = JSON.stringify(JSON.parse(body), null, '  ');
+  console.log('JSON Response\n');
+  console.log(jsonResponse);
+});
