2d208fc4bb997fcf47e3a50c84612695c87ee19d
codehimanshu <himanshuagrawal1998@gmail.com>
2019-11-17 05:56:28 +0530

    add visoin api

--- back-end/services/ocr-service.js
+++ back-end/services/ocr-service.js
@@ -1,12 +1,15 @@
 const request = require('request');
-const config = require('../config');
+// const config = require('../config');
+const vision = require('@google-cloud/vision');
 
 module.exports.getCharacter = getCharacter
 
 function getCharacter(imageString) {
+    const client = new vision.ImageAnnotatorClient();
+
     // let subscriptionKey = config.COMPUTER_VISION_SUBSCRIPTION_KEY;
     // let endpoint = config.COMPUTER_VISION_ENDPOINT
-    let key = config.OCR_KEY
+    // let key = config.OCR_KEY
 
     // if (!subscriptionKey) { throw new Error('Set your environment variables for your subscription key and endpoint.'); }
     
@@ -20,29 +23,32 @@ function getCharacter(imageString) {
     });
     const imageUrl = 'http://168.61.181.153/'+time+'.png';
 
-    // Request parameters.
-    const params = {
-        'language': 'unk',
-        'detectOrientation': 'true',
-    };
+    // // Request parameters.
+    // const params = {
+    //     'language': 'unk',
+    //     'detectOrientation': 'true',
+    // };
     
-    const options = {
-        uri: "https://api.ocr.space/parse/imageurl?apikey="+key+"&url="+imageUrl,
-        // qs: params,
-        // body: '{"url": ' + '"' + imageUrl + '"}',
-        headers: {
-            'Content-Type': 'application/json',
-            // 'Ocp-Apim-Subscription-Key' : subscriptionKey
-        }
-    };
-    
-    request.post(options, (error, response, body) => {
-      if (error) {
-        console.log('Error: ', error);
-        return;
-      }
-      let jsonResponse = JSON.stringify(JSON.parse(body), null, '  ');
-      console.log('JSON Response\n');
-      console.log(jsonResponse);
-    });
+    // const options = {
+    //     uri: "https://api.ocr.space/parse/imageurl?apikey="+key+"&url="+imageUrl,
+    //     // qs: params,
+    //     // body: '{"url": ' + '"' + imageUrl + '"}',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //         // 'Ocp-Apim-Subscription-Key' : subscriptionKey
+    //     }
+    // };
+    const [result] = await client.textDetection("/home/ubuntu/images/"+time+".png");
+const detections = result.textAnnotations;
+console.log('Text:');
+detections.forEach(text => console.log(text));
+    // request.post(options, (error, response, body) => {
+    //   if (error) {
+    //     console.log('Error: ', error);
+    //     return;
+    //   }
+    //   let jsonResponse = JSON.stringify(JSON.parse(body), null, '  ');
+    //   console.log('JSON Response\n');
+    //   console.log(jsonResponse);
+    // });
 }
